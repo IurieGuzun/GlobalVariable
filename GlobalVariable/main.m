7aaad7f6523e02850bfadc79a57d329379740743
Iurie Guzun <iurie_guzun@yahoo.com>
2020-05-30 14:06:09 -0400

    Print myVar from main file

--- GlobalVariable/main.m
+++ GlobalVariable/main.m
@@ -6,12 +6,19 @@
 //  Copyright © 2020 Iurie Guzun. All rights reserved.
 //
 
+int myVar = 321;
+void displayit();
+
 #import <UIKit/UIKit.h>
 #import "AppDelegate.h"
 
 int main(int argc, char * argv[]) {
     NSString * appDelegateClassName;
     @autoreleasepool {
+        
+        NSLog (@"myVar = %i", myVar);
+
+       // displayit();
         // Setup code that might create autoreleased objects goes here.
         appDelegateClassName = NSStringFromClass([AppDelegate class]);
     }
